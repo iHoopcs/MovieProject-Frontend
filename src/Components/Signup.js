6e8ccbad50f7c4cc64f8a14d9cbe4961b5479b7f
Caleb Simmons <97637707+iHoopcs@users.noreply.github.com>
2023-01-30 22:17:44 -0500

    Signup Form
    Base Form created
    POST method needs refinement to send datat to backend

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,5 +1,27 @@
-//Display signup page 
+import './styles.css'; 
 
+//Display signup page 
 export default function Signup(){
-    return <h1>Please fill out form</h1>
+    return (
+        <>
+            <h1>Please fill out signup application form.</h1>
+            <div className='signupForm'>
+                <form action='http://localhost:8080/api/v1/add/user' method='post'>
+                    <label>First Name: </label>
+                    <input type={'text'} placeholder='John' required ></input>
+                    <br></br>
+                    <label>Last Name: </label>
+                    <input type={'text'} placeholder='Doe' required></input>
+                    <br></br>
+                    <label>Email: </label>
+                    <input type={'email'} placeholder='Jdoe@aol.com'required></input>
+                    <br></br>
+                    <label>Age: </label>
+                    <input type={'number'} placeholder='21' required></input>
+                    <br></br>
+                    <input type={'submit'}></input>
+                </form>
+            </div>
+        </>
+    ); 
 }
