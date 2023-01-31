ad19581163fa0ac559d22c48bccb094cd262ddc6
Caleb Simmons <97637707+iHoopcs@users.noreply.github.com>
2023-01-31 17:14:57 -0500

    Retrieve form data
    Successfully get data from form -> send to spring boot app -> save users in db

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -8,16 +8,16 @@ export default function Signup(){
             <div className='signupForm'>
                 <form action='http://localhost:8080/api/v1/add/user' method='post'>
                     <label>First Name: </label>
-                    <input type={'text'} placeholder='John' required ></input>
+                    <input type={'text'} name='firstName' placeholder='John' required ></input>
                     <br></br>
                     <label>Last Name: </label>
-                    <input type={'text'} placeholder='Doe' required></input>
+                    <input type={'text'} name='lastName' placeholder='Doe' required></input>
                     <br></br>
                     <label>Email: </label>
-                    <input type={'email'} placeholder='Jdoe@aol.com'required></input>
+                    <input type={'email'} name='email' placeholder='Jdoe@aol.com'required></input>
                     <br></br>
                     <label>Age: </label>
-                    <input type={'number'} placeholder='21' required></input>
+                    <input type={'number'} name='age' placeholder='21' required></input>
                     <br></br>
                     <input type={'submit'}></input>
                 </form>
