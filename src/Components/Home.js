cae4c9c131ba942035980bcd2f80e2af9e68ce2a
Caleb Simmons <97637707+iHoopcs@users.noreply.github.com>
2023-02-25 16:15:52 -0500

    Homepage Movie Filters
    Display selected type of movies

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -51,7 +51,21 @@ function NavBar(){
                 <h1 className="navBarTitles">ReactMovies.com</h1>
                 <h1><AddMovieButton /></h1>
             </div>  
-            
+
+            <div className='center-filter-links'>
+                <ul>
+                    <li><button className='movie-link-btn'><a href='action' className='style-link-text'>Action</a></button></li>
+                    <li><button className='movie-link-btn'><a href='anime' className='style-link-text'>Anime</a></button></li>
+                    <li><button className='movie-link-btn'><a href='comedy' className='style-link-text'>Comedy</a></button></li>
+                    <li><button className='movie-link-btn'><a href='comingofage' className='style-link-text'>Coming of Age</a></button></li>
+                    <li><button className='movie-link-btn'><a href='drama' className='style-link-text'>Drama</a></button></li>
+                    <li><button className='movie-link-btn'><a href='horror' className='style-link-text'>Horror</a></button></li> 
+                    <li><button className='movie-link-btn'><a href='mysteryscifi' className='style-link-text'>Mystery/Sci-fi</a></button></li>
+                    <li><button className='movie-link-btn'><a href='romance' className='style-link-text'>Romance</a></button></li> 
+                    <li><button className='movie-link-btn'><a href='sport' className='style-link-text'>Sport</a></button></li>  
+                    <li><button className='movie-link-btn'><a href='thriller' className='style-link-text'>Thriller</a></button></li>
+                </ul>        
+            </div>
         </div>
     ); 
 }
