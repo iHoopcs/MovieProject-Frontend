4f370be9ca81be847e2d77bbda417666006f86c5
Caleb Simmons <csimmons718@bears.shawu.edu>
2023-04-09 04:09:56 -0400

    Restyle Card Images

--- src/Components/MovieFrame.js
+++ src/Components/MovieFrame.js
@@ -5,11 +5,14 @@ export default function MovieFrame(props){
     const { movie } = props; 
     return (
         <>
-            <div className='col'>
+            <div className='col-3'>
                 {/*Points to movie specific url endpoint*/}
                 <a href= {`/movies/${movie.id}/${movie.name}`}>
-                    <div className="card mb-5 border-danger ">   
-                        <img src={movie.imgUrl} alt={movie.name}/>
+                    <div className="card mb-5">   
+                        <img src={movie.imgUrl} alt={movie.name} className='card-img-top'/>
+                        <div className='card-body'>
+                            <h6 className='card-title'></h6>
+                        </div>
                     </div>
                 </a>
             </div>
